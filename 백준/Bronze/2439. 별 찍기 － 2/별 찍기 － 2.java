0a40df7e393f43c6cb817b398dbba37ca7f15f2e
huisu73 <165253004+huisu73@users.noreply.github.com>
2026-01-21 12:16:34 +0900

    별 찍기 － 2.java

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -3,14 +3,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        for(int i = 1; i <= N; i++) {		// 행 반복
-            for(int j = 1; j <= N - i; j++){	// 공백 반복
+        for(int i = 1; i <= N; i++) {
+            for(int j = 1; j <= N - i; j++){
                 System.out.print(" ");
             }
-            for(int k = 1; k <= i; k++){	// 별 반복
+            for(int k = 1; k <= i; k++){
                 System.out.print("*");
             }
-            System.out.println();		// 줄 바꿈
+            System.out.println();
         }
     }
+
 }
