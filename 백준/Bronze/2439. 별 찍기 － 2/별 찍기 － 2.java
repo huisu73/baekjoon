4dc3f01fa334e16c66cf0d314c9865d7a1f94546
huisu73 <165253004+huisu73@users.noreply.github.com>
2026-01-21 12:17:09 +0900

    별 찍기 － 2.java

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -13,5 +14,4 @@ public class Main {
             System.out.println();
         }
     }
-
 }
