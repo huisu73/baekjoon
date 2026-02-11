5356ba0368101897e9172bf3ee87a4d8f904ca3b
huisu73 <165253004+huisu73@users.noreply.github.com>
2026-02-11 23:26:44 +0900

    [Silver V] Title: 숫자 카드, Time: 3776 ms, Memory: 359152 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] N_arr = new int[N];
+        HashSet<Integer> N_set = new HashSet<>();
+
+        for (int i = 0; i < N; i++) {
+            N_arr[i] = sc.nextInt();
+            N_set.add(N_arr[i]);
+        }
+
+        int M = sc.nextInt();
+        int[] M_arr = new int[M];
+        
+        for (int i = 0; i < M; i++) {
+            M_arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < M_arr.length; i++) {
+            if (N_set.contains(M_arr[i])) {
+                System.out.print("1 ");
+            } else {
+                System.out.print("0 ");
+            }
+        }
+    }
+}
