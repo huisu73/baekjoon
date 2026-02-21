11d9c827a50f7db1858ce5a18c027f50fa365ac0
huisu73 <165253004+huisu73@users.noreply.github.com>
2026-02-21 23:08:17 +0900

    [Silver V] Title: 카드1, Time: 100 ms, Memory: 14508 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int N = Integer.parseInt(br.readLine());
+    Queue<Integer> q = new LinkedList<>();
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 1; i <= N; i++)
+      q.add(i);
+
+    while (!q.isEmpty()) {
+      sb.append(q.remove());
+      if (!q.isEmpty()) {
+        sb.append(' ');
+        q.add(q.remove());
+      }
+    }
+
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();  
+  }
+}
