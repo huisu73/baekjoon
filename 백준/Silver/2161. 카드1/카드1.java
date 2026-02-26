7f136ec46cadbfb4d0039ebdad9fa54c41e0c16f
huisu73 <165253004+huisu73@users.noreply.github.com>
2026-02-26 11:11:23 +0900

    Update 카드1.java

--- 백준/Silver/2161. 카드1/카드1.java
+++ 백준/Silver/2161. 카드1/카드1.java
@@ -2,27 +2,26 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-  public static void main(String[] args) throws Exception {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    int N = Integer.parseInt(br.readLine());
-    Queue<Integer> q = new LinkedList<>();
-    StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        Queue<Integer> q = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
 
-    for (int i = 1; i <= N; i++)
-      q.add(i);
+        for (int i = 1; i <= N; i++) {
+            q.add(i);
+        }
 
-    while (!q.isEmpty()) {
-      sb.append(q.remove());
-      if (!q.isEmpty()) {
-        sb.append(' ');
-        q.add(q.remove());
-      }
+        while (!q.isEmpty()) {
+            sb.append(q.poll());
+
+            if (!q.isEmpty()) {
+                sb.append(' ');
+                q.add(q.poll());
+            }
+        }
+
+        System.out.print(sb.toString());
     }
-
-    bw.write(sb.toString());
-    bw.flush();
-  }
-
 }
