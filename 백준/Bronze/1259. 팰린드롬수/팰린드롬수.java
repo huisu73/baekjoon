a1480c085e638f466caf4ec485855156051fac55
huisu73 <165253004+huisu73@users.noreply.github.com>
2026-02-26 14:37:00 +0900

    Update 팰린드롬수.java

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -1,29 +1,23 @@
-import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         while (true) {
             String str = br.readLine();
+            if (str.equals("0")) break;
+
             boolean check = true;
-            
-            if (str.equals("0")) {
-                break;
-            }
-            
+
             for (int i = 0; i < str.length() / 2; i++) {
                 if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
                     check = false;
+                    break;
                 }
             }
-            
-            if (check) {
-                System.out.println("yes");
-            } else {
-                System.out.println("no");
-            }
+
+            System.out.println(check ? "yes" : "no");
         }
     }
 }
