3bcaa625401025e54fc624b09230f4d32d5bf120
huisu73 <165253004+huisu73@users.noreply.github.com>
2026-02-26 10:17:08 +0900

    Update 괄호.java

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -4,39 +4,31 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int T = Integer.parseInt(br.readLine());
+        
         for(int i=0; i<T; i++) {
             String ps = br.readLine();
-
-            bw.write(isVPS(ps) + "\n");
+            System.out.println(isVPS(ps));
         }
-
-        bw.flush();
     }
 
     public static String isVPS(String ps) {
         Stack<Character> stack = new Stack<>();
 
         for(int i=0; i<ps.length(); i++) {
-            char psChar = ps.charAt(i);
+            char c = ps.charAt(i);
 
-            if(psChar == '(') {
-                stack.push(psChar);
-            }
-            else if(psChar == ')' && !stack.empty()) {
+            if(c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.empty()) {
+                    return "NO";
+                }
                 stack.pop();
             }
-            else if(psChar == ')' && stack.empty()) {
-                return "NO";
-            }
         }
 
-        if(stack.empty()) {
-            return "YES";
-        } else {
-            return "NO";
-        }
+        return stack.empty() ? "YES" : "NO";
     }
 }
